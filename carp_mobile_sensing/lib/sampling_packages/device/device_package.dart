bbbcdbfd3303eb1df4ebf97141bf05dbaac5535b
bardram <jakba@dtu.dk>
2019-12-19 15:46:00 +0100

    v. 0.6.2+1

--- carp_mobile_sensing/lib/sampling_packages/device/device_package.dart
+++ carp_mobile_sensing/lib/sampling_packages/device/device_package.dart
@@ -30,7 +30,7 @@ class DeviceSamplingPackage implements SamplingPackage {
 
   void onRegister() {} // does nothing for this device sampling package
 
-  List<PermissionGroup> get permissions => [PermissionGroup.phone];
+  List<PermissionGroup> get permissions => [];
 
   SamplingSchema get common => SamplingSchema()
     ..type = SamplingSchemaType.COMMON
