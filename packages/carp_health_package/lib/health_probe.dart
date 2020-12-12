ee52a8717856f15bc0c9a8e54df9ddebf7cf5aef
thomasnilsson <tnk.videnskab@gmail.com>
2020-12-12 13:25:36 +0100

    Added request authorization to the initialize method

--- packages/carp_health_package/lib/health_probe.dart
+++ packages/carp_health_package/lib/health_probe.dart
@@ -1,7 +1,8 @@
 part of health_package;
 
 class HealthProbe extends StreamProbe {
-  StreamController<HealthDatum> _ctrl = StreamController<HealthDatum>.broadcast();
+  StreamController<HealthDatum> _ctrl =
+      StreamController<HealthDatum>.broadcast();
 
   Stream<HealthDatum> get stream => _ctrl.stream;
   HealthMeasure healthMeasure;
@@ -11,8 +12,12 @@ class HealthProbe extends StreamProbe {
 
   void onInitialize(Measure measure) {
     super.onInitialize(measure);
-    assert(measure is HealthMeasure, 'An HealthMeasure must be provided to use the HealthProbe.');
+    assert(measure is HealthMeasure,
+        'An HealthMeasure must be provided to use the HealthProbe.');
     healthMeasure = (measure as HealthMeasure);
+
+    /// Request access to the health data type before starting sampling
+    _healthFactory.requestAuthorization([healthMeasure.healthDataType]);
   }
 
   Future<void> onResume() async {
@@ -20,15 +25,19 @@ class HealthProbe extends StreamProbe {
 
     debug('healthMeasure : $healthMeasure');
 
-    DateTime start = healthMeasure.lastTime ?? DateTime.now().subtract(healthMeasure.history);
+    DateTime start = healthMeasure.lastTime ??
+        DateTime.now().subtract(healthMeasure.history);
     DateTime end = DateTime.now();
     HealthDataType healthDataType = healthMeasure.healthDataType;
     List<HealthDataPoint> data = List<HealthDataPoint>();
 
-    debug('Collecting health data - type: $healthDataType, start: ${start.toUtc()}, end: ${end.toUtc()}');
+    debug(
+        'Collecting health data - type: $healthDataType, start: ${start.toUtc()}, end: ${end.toUtc()}');
     try {
-      data = await _healthFactory.getHealthDataFromTypes(start, end, [healthDataType]);
-      debug('Retrieved ${data.length} health data points of type. $healthDataType');
+      data = await _healthFactory
+          .getHealthDataFromTypes(start, end, [healthDataType]);
+      debug(
+          'Retrieved ${data.length} health data points of type. $healthDataType');
 
       // convert [HealthDataPoint] to Datums and add them to the stream.
       data.forEach((data) => _ctrl.add(HealthDatum.fromHealthDataPoint(data)));
