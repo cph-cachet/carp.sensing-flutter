8f436fbea4ee27d9a9b05cfd5eb5b3a047ccc362
bardram <jakba@dtu.dk>
2021-05-11 23:01:20 +0200

    removal of test project

--- apps/carp/bin/carp.dart
+++
@@ -1,7 +0,0 @@
-import 'package:carp/carp.dart' as carp;
-import 'package:carp_backend/carp_backend.dart';
-
-void main(List<String> arguments) {
-  print('Hello world: ${carp.calculate()}!');
-  print(CarpResourceManager());
-}
--- apps/carp/test/carp_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:carp/carp.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
