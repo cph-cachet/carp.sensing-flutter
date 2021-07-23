96ef640bdf84edf532581a10e585fc98dfb8e926
bardram <jakba@dtu.dk>
2021-07-23 16:07:07 +0200

    carp_firebase_backend 0.30.0 published

--- backends/carp_firebase_backend/lib/carp_firebase_backend.dart
+++ backends/carp_firebase_backend/lib/carp_firebase_backend.dart
@@ -9,9 +9,18 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:json_annotation/json_annotation.dart';
+
+import 'package:carp_core/carp_core.dart';
 import 'package:carp_mobile_sensing/carp_mobile_sensing.dart';
 
 part 'firebase/firebase_common.dart';
 part 'firebase/firebase_data_endpoint.dart';
 part 'firebase/firebase_data_managers.dart';
 part 'carp_firebase_backend.g.dart';
+
+/// Generic CARP Firebase Backend exception.
+class CarpFirebaseBackendException implements Exception {
+  dynamic message;
+  CarpFirebaseBackendException([this.message]);
+  String toString() => '$runtimeType - $message';
+}
--- backends/carp_firebase_backend/lib/firebase/firebase_data_endpoint.dart
+++ backends/carp_firebase_backend/lib/firebase/firebase_data_endpoint.dart
@@ -10,57 +10,64 @@ part of carp_firebase_backend;
 /// Specify a Google Firebase endpoint.
 @JsonSerializable(fieldRename: FieldRename.snake, includeIfNull: false)
 class FirebaseEndPoint {
-  /// The name of the Firebase endpoint.
-  /// Can be anything, but its recommended to name it according to the Firebase bucket.
+  /// The name of the Firebase endpoint. Can be anything, but its recommended
+  /// to name it according to the Firebase bucket.
   String name;
 
   /// The URI of the Firebase endpoint.
   String uri;
 
-  /// The authentization method used for Firebase. See [FireBaseAuthenticationMethods] for options.
+  /// The authentization method used for Firebase.
+  /// See [FireBaseAuthenticationMethods] for options.
   ///
-  /// See [Firebase Authentication](https://firebase.google.com/docs/auth/) for a list of authentication options.
+  /// See [Firebase Authentication](https://firebase.google.com/docs/auth/)
+  /// for a list of authentication options.
   String firebaseAuthenticationMethod;
 
   /// Email (used as username) if using password authentication.
-  String email;
+  String? email;
 
   /// Password if using password authentication.
   ///
   /// TODO : right now in clear text -- not so good.
-  String password;
+  String? password;
 
   /// Custom token, if using custom auth system integration.
-  String token;
+  String? token;
 
-  /// The Firebase project ID (not the Name!). See Firebase project settings for this ID.
+  /// The Firebase project ID (not the Name!).
+  /// See Firebase project settings for this ID.
   String projectID;
 
-  /// The Firebase Web API Key. See Firebase project settings for this ID.
+  /// The Firebase Web API Key.
+  /// See Firebase project settings for this ID.
   String webAPIKey;
 
-  /// The Firebase App ID for Android. Should be set up in the Firebase project settings.
+  /// The Firebase App ID for Android.
+  /// Should be set up in the Firebase project settings.
   String androidGoogleAppID;
 
-  /// The Firebase App ID for iOS. Should be set up in the Firebase project settings.
+  /// The Firebase App ID for iOS.
+  /// Should be set up in the Firebase project settings.
   String iOSGoogleAppID;
 
-  // The Firebase GCM (Google Cloud Messaging) Sender ID. See project setting under the 'Cloud Messaging' tab.
+  // The Firebase GCM (Google Cloud Messaging) Sender ID.
+  // See project setting under the 'Cloud Messaging' tab.
   String gcmSenderID;
 
   /// Creates a [FirebaseEndPoint].
   FirebaseEndPoint(
-      {this.name,
-      this.uri,
-      this.firebaseAuthenticationMethod,
+      {required this.name,
+      required this.uri,
+      required this.firebaseAuthenticationMethod,
       this.email,
       this.password,
       this.token,
-      this.projectID,
-      this.webAPIKey,
-      this.androidGoogleAppID,
-      this.iOSGoogleAppID,
-      this.gcmSenderID})
+      required this.projectID,
+      required this.webAPIKey,
+      required this.androidGoogleAppID,
+      required this.iOSGoogleAppID,
+      required this.gcmSenderID})
       : super();
 
   static Function get fromJsonFunction => _$FirebaseEndPointFromJson;
@@ -71,7 +78,7 @@ class FirebaseEndPoint {
 
 abstract class FirebaseDataEndPoint {
   /// The Firebase endpoint.
-  FirebaseEndPoint firebaseEndPoint;
+  late FirebaseEndPoint firebaseEndPoint;
 }
 
 /// Specify a Google Firebase Database (Cloud Firestore) document endpoint.
@@ -81,16 +88,18 @@ abstract class FirebaseDataEndPoint {
 class FirebaseDatabaseDataEndPoint extends DataEndPoint
     with FirebaseDataEndPoint {
   /// When uploading to the Firebase Database using the [FirebaseDatabaseDataManager],
-  /// [collection] hold the name of the collection to store json objects. May contain sub-collections separated with `/`.
+  /// [collection] hold the name of the collection to store json objects.
+  /// May contain sub-collections separated with `/`.
   ///
-  /// JSON objects will be stored in collections named <study_id>/<device_id> relative to this path.
-  /// For example, if collection = "carp_data", study_id = "1234" and device_id = "987234", the data will
-  /// be stored as documents in "carp_data/1234/987234/".
+  /// JSON objects will be stored in collections named <study_id>/<device_id>
+  /// relative to this path.
+  /// For example, if collection = "carp_data", study_id = "1234" and device_id = "987234",
+  /// the data will be stored as documents in "carp_data/1234/987234/".
   String collection;
 
   /// Creates a [FirebaseDatabaseDataEndPoint].
   FirebaseDatabaseDataEndPoint(FirebaseEndPoint firebaseEndPoint,
-      {this.collection})
+      {required this.collection})
       : super(type: DataEndPointTypes.FIREBASE_DATABSE) {
     this.firebaseEndPoint = firebaseEndPoint;
   }
@@ -103,21 +112,28 @@ class FirebaseDatabaseDataEndPoint extends DataEndPoint
 
 /// Specify a Google Firebase Storage file endpoint.
 ///
-/// See [Firebase Storage](https://firebase.google.com/docs/storage) for description of Firebase file storage.
+/// See [Firebase Storage](https://firebase.google.com/docs/storage) for
+/// description of Firebase file storage.
 @JsonSerializable(fieldRename: FieldRename.snake, includeIfNull: false)
 class FirebaseStorageDataEndPoint extends FileDataEndPoint
     with FirebaseDataEndPoint {
   /// When uploading to the Firebase Storage using the [FirebaseStorageDataManager],
-  /// [path] hold the folder path where to store files. May contain sub-folders separated with `/`.
+  /// [path] hold the folder path where to store files. May contain sub-folders
+  /// separated with `/`.
   ///
-  /// Data (zip files) will be stored in folders named <study_id>/<device_id> relative to this path.
-  /// For example, if path = "sensing/data", study_id = "1234" and device_id = "987234", the data will
-  /// be stored in "sensing/data/1234/987234/".
+  /// Data (zip files) will be stored in folders named <study_id>/<device_id>
+  /// relative to this path.
+  /// For example, if path = "sensing/data", study_id = "1234" and device_id = "987234",
+  /// the data will be stored in "sensing/data/1234/987234/".
   String path;
 
   /// Creates a [FirebaseStorageDataEndPoint].
   FirebaseStorageDataEndPoint(FirebaseEndPoint firebaseEndPoint,
-      {this.path, bufferSize, zip, encrypt, publicKey})
+      {required this.path,
+      required bufferSize,
+      zip = false,
+      encrypt = false,
+      publicKey})
       : super(
             type: DataEndPointTypes.FIREBASE_STORAGE,
             bufferSize: bufferSize,
