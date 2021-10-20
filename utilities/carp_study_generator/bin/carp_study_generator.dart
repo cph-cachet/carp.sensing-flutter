1612646b524cde844b8f29e20c57e33cfbc38e5f
bardram <jakba@dtu.dk>
2021-10-20 19:29:53 +0200

    carp_study_generator 0.31.0+1 published

--- utilities/carp_study_generator/bin/carp_study_generator.dart
+++ utilities/carp_study_generator/bin/carp_study_generator.dart
@@ -1,5 +1,4 @@
-import 'package:carp_study_generator/carp_study_generator.dart';
-import 'package:test/test.dart';
+// import 'package:carp_study_generator/carp_study_generator.dart';
 
 Future<void> main(List<String> args) async {
   print('Hello World!');
