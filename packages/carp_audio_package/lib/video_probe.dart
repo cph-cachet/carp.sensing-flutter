7f880ecc291f7cd7e660146898254c8468e2e369
bardram <jakba@dtu.dk>
2021-12-16 13:14:21 +0100

    Update video_probe.dart

--- packages/carp_audio_package/lib/video_probe.dart
+++ packages/carp_audio_package/lib/video_probe.dart
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Copenhagen Center for Health Technology (CACHET) at the
+ * Copyright 2021 Copenhagen Center for Health Technology (CACHET) at the
  * Technical University of Denmark (DTU).
  * Use of this source code is governed by a MIT-style license that can be
  * found in the LICENSE file.
@@ -8,13 +8,8 @@ part of audio;
 
 /// A probe that captures a video or image.
 ///
-/// This probes does not in itself capture the video/image. It merely takes
-/// a video/image file and wraps it into a [VideoDatum].
-///
-/// The video probe generates an [VideoDatum] that holds the meta-data for each
-/// recording along with the actual recording in an video file.
-/// How to upload this data to a data backend is up to the implementation of the
-/// [DataManager], which is used in the [Study].
+/// This probes does not in itself capture the video/image. It merely is a placeholder
+/// for being able to add a video measure to a protocol.
 class VideoProbe extends DatumProbe {
   @override
   Future<Datum?> getDatum() async =>
