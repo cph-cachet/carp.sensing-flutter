08a8db324274e292535dcefc9d17987c184430e8
sc00n <48179232+sc00n@users.noreply.github.com>
2023-10-03 10:17:28 +0200

    Update sensors.g.dart
    Update for code in sensor_data.dart

--- carp_mobile_sensing/lib/sampling_packages/sensors/sensors.g.dart
+++ carp_mobile_sensing/lib/sampling_packages/sensors/sensors.g.dart
@@ -34,3 +34,41 @@ Map<String, dynamic> _$AmbientLightToJson(AmbientLight instance) {
   writeNotNull('maxLux', instance.maxLux);
   return val;
 }
+
+AverageAccelerometer _$AverageAccelerometerFromJson(
+        Map<String, dynamic> json) =>
+    AverageAccelerometer(
+      xm: (json['xm'] as num?)?.toDouble(),
+      ym: (json['ym'] as num?)?.toDouble(),
+      zm: (json['zm'] as num?)?.toDouble(),
+      xms: (json['xms'] as num?)?.toDouble(),
+      yms: (json['yms'] as num?)?.toDouble(),
+      zms: (json['zms'] as num?)?.toDouble(),
+      n: json['n'] as int?,
+    )
+      ..$type = json['__type'] as String?
+      ..sensorSpecificData = json['sensorSpecificData'] == null
+          ? null
+          : Data.fromJson(json['sensorSpecificData'] as Map<String, dynamic>);
+
+Map<String, dynamic> _$AverageAccelerometerToJson(
+    AverageAccelerometer instance) {
+  final val = <String, dynamic>{};
+
+  void writeNotNull(String key, dynamic value) {
+    if (value != null) {
+      val[key] = value;
+    }
+  }
+
+  writeNotNull('__type', instance.$type);
+  writeNotNull('sensorSpecificData', instance.sensorSpecificData);
+  writeNotNull('xm', instance.xm);
+  writeNotNull('ym', instance.ym);
+  writeNotNull('zm', instance.zm);
+  writeNotNull('xms', instance.xms);
+  writeNotNull('yms', instance.yms);
+  writeNotNull('zms', instance.zms);
+  writeNotNull('n', instance.n);
+  return val;
+}
